7f23edebcb71631bec266dd499c15fa9e895ff0d
khanshorif331 <khanshorif331@gmail.com>
2022-09-03 23:23:26 +0600

    routes updated

--- routes/usersRoutes/users.routes.js
+++ routes/usersRoutes/users.routes.js
@@ -1,17 +1,16 @@
 //dependencies
-const express = require("express");
-const { getRandomUser, getAllUsers, postUser, updateUser, bulkUpdate, deleteUser } = require("../../controllers/users.controller");
+const express = require('express')
+const {
+	getRandomUser,
+	getAllUsers,
+	postUser,
+	updateUser,
+	bulkUpdate,
+	deleteUser,
+} = require('../../controllers/users.controller')
 
 //module scaffolding
-const usersRoutes = express.Router();
-
-//routes
-usersRoutes.route("/user/random").get(getRandomUser);
-usersRoutes.route("/user/all").get(getAllUsers);
-usersRoutes.route("/user/save").post(postUser);
-usersRoutes.route("/user/update").patch(updateUser);
-usersRoutes.route("/user/bulk-update").patch(bulkUpdate);
-usersRoutes.route("/user/delete").delete(deleteUser);
+const usersRoutes = express.Router()
 
 //export module
-module.exports = usersRoutes;
+module.exports = usersRoutes
