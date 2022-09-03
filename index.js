0683cf3548c037fc7029f0d39c92bc5b1fdfb8fe
khanshorif331 <khanshorif331@gmail.com>
2022-09-03 19:36:34 +0300

    index file done

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 //dependencies
-const express = require("express");
-const cors = require("cors");
-const { errorHandler } = require("./middlewares/errorHandler");
-const usersRoutes = require("./routes/usersRoutes/users.routes");
+const express = require('express')
+const cors = require('cors')
+const { errorHandler } = require('./middlewares/errorHandler')
+const usersRoutes = require('./routes/usersRoutes/users.routes')
 
 //app config
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
 //middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-//handle application errors
+//handle application errors here
 app.use(errorHandler)
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Welcome to Random User Api",
-    });
+app.get('/', (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: 'Welcome to Random User Api',
+	})
 })
 
-// dynamic api routes
-app.use("/api/v1", usersRoutes );
+// dynamic api routes here
+app.use('/api/v1', usersRoutes)
 
-//create server
-app.listen(port, () => console.log(`Listening on Port: ${port}`));
+//create server here
+app.listen(port, () => console.log(`Listening on Port: ${port}`))
 
-// handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  console.log({
-    name: err.name,
-    message: err.message,
-    stack: err.stack,
-  });
-  // close server & exit process
-  app.close(() => process.exit(1));
-});
+// handle unhandled promise rejections here
+process.on('unhandledRejection', (err, promise) => {
+	console.log({
+		name: err.name,
+		message: err.message,
+		stack: err.stack,
+	})
+	// close server
+	app.close(() => process.exit(1))
+})
