38bd098f88651000453b39a6db0e72503331dad9
khanshorif331 <khanshorif331@gmail.com>
2022-09-03 23:24:09 +0600

    router setup done

--- routes/usersRoutes/users.routes.js
+++ routes/usersRoutes/users.routes.js
@@ -12,5 +12,13 @@ const {
 //module scaffolding
 const usersRoutes = express.Router()
 
+//routes
+usersRoutes.route('/user/random').get(getRandomUser)
+usersRoutes.route('/user/all').get(getAllUsers)
+usersRoutes.route('/user/save').post(postUser)
+usersRoutes.route('/user/update').patch(updateUser)
+usersRoutes.route('/user/bulk-update').patch(bulkUpdate)
+usersRoutes.route('/user/delete').delete(deleteUser)
+
 //export module
 module.exports = usersRoutes
